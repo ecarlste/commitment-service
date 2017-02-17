6ee712976ed76841e815c3f2104d0fcdee6050c3
Erik S Carlsten <ecarlste@gmail.com>
2017-02-16 18:45:46 -0600

    Initial commit

---
+++ src/main/java/com/erikcarlsten/commitoften/CommitmentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.erikcarlsten.commitoften;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitmentServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/erikcarlsten/commitoften/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.erikcarlsten.commitoften;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommitmentServiceApplication.class);
+	}
+
+}
